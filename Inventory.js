045d009b69b541da0af935186f8dd62c14013b0d
James Hobin <hobinjk@gmail.com>
2025-03-16 17:58:04 -0400

    Add basic inventory menu

---
+++ Inventory.js
@@ -0,0 +1,104 @@
+import { Prop } from './Prop.js';
+
+export class Inventory {
+  constructor(settings, actionManager, propTemplates) {
+    this.settings = settings;
+    this.actionManager = actionManager;
+    this.propTemplates = propTemplates;
+    this.propTemplatesById = {};
+    this.nextPTId = 0;
+
+    this.container = document.createElement('div');
+    this.container.classList.add('inventory');
+    this.open = false;
+
+    this.activeDrag = null;
+    this.onPropTemplatePointerDown = this.onPropTemplatePointerDown.bind(this);
+    this.onPointerMove = this.onPointerMove.bind(this);
+    this.onPointerUp = this.onPointerUp.bind(this);
+
+    window.addEventListener('pointermove', this.onPointerMove, { passive: true });
+    window.addEventListener('pointerup', this.onPointerUp, { passive: true });
+
+
+    this.addPropTemplates();
+  }
+
+  addPropTemplates() {
+    for (let propTemplate of this.propTemplates) {
+      this.addPropTemplate(propTemplate);
+    }
+  }
+
+  addPropTemplate(propTemplate) {
+    let propTemplateContainer = propTemplate.create();
+    propTemplateContainer.dataset.id = this.nextPTId;
+    this.propTemplatesById[this.nextPTId] = propTemplate;
+    this.nextPTId += 1;
+
+    propTemplateContainer.addEventListener('pointerdown', this.onPropTemplatePointerDown);
+    this.container.appendChild(propTemplateContainer);
+  }
+
+  onPropTemplatePointerDown(event) {
+    let ptId = event.target.dataset.id;
+    if (!ptId) {
+      console.warn('event without ptId', event);
+      return;
+    }
+    let pt = this.propTemplatesById[ptId];
+    if (!pt) {
+      console.warn('ptId refers to missing prop', event, this.propTemplatesById);
+      return;
+    }
+    let x = event.clientX;
+    let y = event.clientY;
+    if (this.settings.snapToGrid) {
+      x = this.settings.snapToGrid(x);
+      y = this.settings.snapToGrid(y);
+    }
+
+    let prop = new Prop(x, y, pt);
+    this.activeDrag = {
+      prop,
+    };
+
+    this.bounds = this.container.getBoundingClientRect();
+    this.container.classList.add('trash');
+  }
+
+  onPointerMove(event) {
+    if (!this.activeDrag) {
+      return;
+    }
+    let x = event.clientX;
+    let y = event.clientY;
+    if (this.settings.snapToGrid) {
+      x = this.settings.snapToGrid(x);
+      y = this.settings.snapToGrid(y);
+    }
+    let { prop } = this.activeDrag;
+    prop.moveTo(x - prop.width / 2, y - prop.height / 2);
+  }
+
+  onPointerUp(event) {
+    if (!this.activeDrag) {
+      return;
+    }
+    let x = event.clientX;
+    let y = event.clientY;
+
+    let prop = this.activeDrag.prop;
+    this.activeDrag = null;
+    this.container.classList.remove('trash');
+
+    if (x > this.bounds.left && x < this.bounds.right &&
+      y > this.bounds.top && y < this.bounds.bottom) {
+      // Cancel, prop was dropped in the trash
+      prop.remove();
+      return;
+    }
+
+    this.actionManager.addProp(prop);
+  }
+}
--- cats.js
+++ cats.js
@@ -1,29 +1,36 @@
 import { ActionManager } from './ActionManager.js';
+import { Inventory } from './Inventory.js';
 import { Neko, catNames } from './Neko.js';
 import { Prop } from './Prop.js';
-import { BedTemplate, BookshelfTemplate } from './PropTemplate.js';
+import { bedTemplate, bookshelfTemplate } from './PropTemplate.js';
 
 
 const cats = catNames.map(name => new Neko(name));
 const props = [];
-const bedTemplate = new BedTemplate();
-const bsTemplate = new BookshelfTemplate();
-
-for (let i = 0; i < 10; i++) {
-  let x = Math.round((Math.random() * 0.8 + 0.1) * window.innerWidth / 16) * 16;
-  let y = Math.round((Math.random() * 0.8 + 0.1) * window.innerHeight / 16) * 16;
-
-  props.push(new Prop(x, y, bsTemplate));
-}
-
-for (let i = 0; i < 10; i++) {
-  let x = Math.round((Math.random() * 0.8 + 0.1) * window.innerWidth / 16) * 16;
-  let y = Math.round((Math.random() * 0.8 + 0.1) * window.innerHeight / 16) * 16;
-
-  props.push(new Prop(x, y, bedTemplate));
-}
 
 let actionManager = new ActionManager(cats, props, false);
+
+const inventory = new Inventory({
+  snapToGrid: false,
+}, actionManager, [
+  bedTemplate,
+  bookshelfTemplate,
+]);
+document.body.appendChild(inventory.container);
+
+// for (let i = 0; i < 10; i++) {
+//   let x = Math.round((Math.random() * 0.8 + 0.1) * window.innerWidth / 16) * 16;
+//   let y = Math.round((Math.random() * 0.8 + 0.1) * window.innerHeight / 16) * 16;
+//
+//   props.push(new Prop(x, y, bsTemplate));
+// }
+//
+// for (let i = 0; i < 10; i++) {
+//   let x = Math.round((Math.random() * 0.8 + 0.1) * window.innerWidth / 16) * 16;
+//   let y = Math.round((Math.random() * 0.8 + 0.1) * window.innerHeight / 16) * 16;
+//
+//   props.push(new Prop(x, y, bedTemplate));
+// }
 function update() {
   actionManager.update();
   window.requestAnimationFrame(update);
